6abbd686b96204ca6573ec9974096d4d0ea825dd
mehdiviolet <mehdiviolet@yahoo.com>
2024-04-05 20:18:10 +0200

    add modify

--- src/App.js
+++ src/App.js
@@ -24,6 +24,8 @@ const initialFriends = [
 export default function App() {
   const [friends, setFriends] = useState(initialFriends);
   const [isOpen, setIsOpen] = useState(false);
+  const [selectedFriend, setSelectedFriend] = useState(null);
+
   function handlerBtn() {
     setIsOpen((show) => !show);
   }
@@ -32,16 +34,22 @@ export default function App() {
     setFriends((friens) => [...friens, newFriend]);
     setIsOpen(false);
   }
+
+  function handleSelection(friend) {
+    setSelectedFriend(friend);
+    console.log(friend);
+  }
+
   return (
     <div className="app">
       <div className="sidebar">
-        <FriendsList friends={friends} />
+        <FriendsList friends={friends} onSelection={handleSelection} />
         {isOpen && <FormAddFriend onSetFriends={handlerAddFriend} />}
         <Button onHandlerBtn={handlerBtn}>
           {!isOpen ? "Add friends" : "close"}
         </Button>
       </div>
-      <FormSlitBill />
+      {selectedFriend && <FormSlitBill selectedFriend={selectedFriend} />}
     </div>
   );
 }
@@ -54,17 +62,17 @@ function Button({ children, onHandlerBtn }) {
   );
 }
 
-function FriendsList({ friends }) {
+function FriendsList({ friends, onSelection }) {
   return (
     <ul>
       {friends.map((friend) => (
-        <Friend friend={friend} key={friend.id} />
+        <Friend friend={friend} key={friend.id} onSelection={onSelection} />
       ))}
     </ul>
   );
 }
 
-function Friend({ friend }) {
+function Friend({ friend, onSelection }) {
   return (
     // <>
     <li>
@@ -81,7 +89,7 @@ function Friend({ friend }) {
         </p>
       )}
       {friend.balance === 0 && <p>You and {friend.name} are even</p>}
-      <Button>select</Button>
+      <Button onClick={() => onSelection(friend)}>select</Button>
     </li>
     // </>
   );
@@ -127,7 +135,7 @@ function FormAddFriend({ onSetFriends }) {
 function FormSlitBill() {
   return (
     <form className="form-split-bill">
-      <h2>split a bill with X</h2>
+      <h2>split a bill with selectedFriend</h2>
       <label>💰 Bill value</label>
       <input type="text" />
 
