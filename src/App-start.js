5290c3bbb7ee71eacad6fae1922195bf08520613
mehdiviolet <mehdiviolet@yahoo.com>
2024-06-24 12:45:38 +0200

    add mody

--- src/App-start.js
+++ src/App-start.js
@@ -64,7 +64,7 @@ function FormSplitBill() {
       <label>💰 Bill value</label>
       <input type="text" />
 
-      <label>🧍‍♀️Your expense</label>
+      <label>🧍Your expense</label>
       <input type="text" />
 
       <label>👫friend's expense</label>
