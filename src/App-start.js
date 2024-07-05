ee7b21d8ad4d91934af14c076a2c6dbdf31b5513
mehdiviolet <mehdiviolet@yahoo.com>
2024-07-05 08:46:35 +0200

    add mody

--- src/App-start.js
+++ src/App-start.js
@@ -67,10 +67,10 @@ function FormSplitBill() {
       <label>🧍Your expense</label>
       <input type="text" />
 
-      <label>👫 friend's expense</label>
+      <label>👫friend's expense</label>
       <input type="text" />
 
-      <label>🤑 Who is paying the bill</label>
+      <label>🤑Who is paying the bill</label>
       <select>
         <option value="user"> You</option>
         <option value="friend"> name</option>
